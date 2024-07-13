168ee819dfd3c13ec60a20b14576b5c29f3f5750
James McFadden <998911+mcfdn@users.noreply.github.com>
2024-07-13 14:02:39 +0100

    luasnip

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,8 +1,19 @@
 return {
     "hrsh7th/nvim-cmp",
+    dependencies = {
+        "L3MON4D3/LuaSnip",
+        "hrsh7th/cmp-nvim-lsp",
+    },
     config = function()
         local cmp = require("cmp")
+        local luasnip = require("luasnip")
+
         cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
             mapping = cmp.mapping.preset.insert({
                 ["<C-k>"] = cmp.mapping.select_prev_item(),
                 ["<C-j>"] = cmp.mapping.select_next_item(),
@@ -13,6 +24,7 @@ return {
             }),
             sources = {
                 { name = "nvim_lsp" },
+                { name = "luasnip" },
             },
         })
     end,
