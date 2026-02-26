e263367d90dce2cea9ec9df568be8d64e49be80b
James McFadden <998911+mcfdn@users.noreply.github.com>
2026-02-26 20:36:01 +0000

    format

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -56,28 +56,30 @@ return {
             },
         }
 
-dap.adapters.codelldb = {
-      type = "server",
-      port = "${port}",
-      executable = {
-        command = "codelldb", -- if installed globally or via Mason
-        args = { "--port", "${port}" },
-      },
-    }
+        dap.adapters.codelldb = {
+            type = "server",
+            port = "${port}",
+            executable = {
+                command = "codelldb",
+                args = { "--port", "${port}" },
+            },
+        }
 
-    dap.configurations.rust = {
-      {
-        name = "Launch Rust binary",
-        type = "codelldb",
-        request = "launch",
-        program = function()
-          return vim.fn.input("Path to executable: ",
-            vim.fn.getcwd() .. "/target/debug/",
-            "file")
-        end,
-        cwd = "${workspaceFolder}",
-        stopOnEntry = false,
-      },
-    }
+        dap.configurations.rust = {
+            {
+                name = "Launch Rust binary",
+                type = "codelldb",
+                request = "launch",
+                program = function()
+                  return vim.fn.input(
+                      "Path to executable: ",
+                      vim.fn.getcwd() .. "/target/debug/",
+                      "file"
+                  )
+                end,
+                cwd = "${workspaceFolder}",
+                stopOnEntry = false,
+            },
+        }
     end,
 }
