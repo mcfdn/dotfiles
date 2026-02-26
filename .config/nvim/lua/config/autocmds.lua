cf2746ebe1e3df6c1d23b38911103c41c780afd7
James McFadden <998911+mcfdn@users.noreply.github.com>
2026-02-26 20:27:34 +0000

    adjust lsp-attach autocmds
    removes some unused bindings and avoids inlay hints

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -24,14 +24,17 @@ vim.api.nvim_create_autocmd("FileType", {
 
 vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(event)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = event.buf })
-        vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, { buffer = event.buf })
 
-        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = event.buf })
-        vim.keymap.set("n", "<leader>c", vim.lsp.buf.code_action, { buffer = event.buf })
+        -- No inlay hints ever.
+        vim.lsp.inlay_hint.enable(false, { bufnr = event.buf })
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client then
+          client.server_capabilities.inlayHintProvider = nil
+        end
 
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = event.buf })
+        local opts = { buffer = event.buf }
 
-        vim.keymap.set("n", "<leader>l", vim.lsp.buf.format, { buffer = event.buf })
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "<leader>l", vim.lsp.buf.format, opts)
     end,
 })
