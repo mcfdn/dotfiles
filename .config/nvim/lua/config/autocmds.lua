c242a78fad02482bbc4adb64540aebf0bcfaafc8
James McFadden <998911+mcfdn@users.noreply.github.com>
2026-03-26 20:34:16 +0000

    tidy autocmds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,24 +1,16 @@
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "markdown" },
+    pattern = { "markdown", "go" },
     callback = function()
-        vim.opt.textwidth = 80
-        vim.opt.colorcolumn = "80"
-    end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "go" },
-    callback = function()
-        vim.opt.textwidth = 80
-        vim.opt.colorcolumn = "80"
+        vim.opt_local.textwidth = 80
+        vim.opt_local.colorcolumn = "80"
     end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "rust" },
     callback = function()
-        vim.opt.textwidth = 100
-        vim.opt.colorcolumn = "100"
+        vim.opt_local.textwidth = 100
+        vim.opt_local.colorcolumn = "100"
     end,
 })
 
