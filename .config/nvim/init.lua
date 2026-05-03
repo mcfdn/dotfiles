682c0f53ae7471187318c3135cc1781492be9d3d
James McFadden <998911+mcfdn@users.noreply.github.com>
2026-05-03 09:47:35 +0100

    single init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,159 @@
-require("config")
+-- Prerequisites:
+-- cargo install tree-sitter-cli
+-- go install golang.org/x/tools/gopls@latest
+-- rustup component add rust-analyzer
 
--- vim: ts=2 sts=2 sw=2 et
+local vim = vim
+local undodir = vim.fn.stdpath("data") .. "/undo"
+
+vim.fn.mkdir(undodir, "p")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.wrap = false
+vim.o.cursorline = true
+vim.o.hlsearch = false
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.number = true
+vim.o.rnu = true
+vim.o.scrolloff = 10
+vim.o.clipboard = "unnamedplus"
+vim.o.undofile = true
+vim.o.undodir = undodir
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+vim.o.termguicolors = true
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.signcolumn = "yes:2" -- vim-gitgutter, diagnostics
+vim.o.list = true
+vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.listchars = { tab = "→ ", trail = "·" }
+vim.opt.path:append("**")
+
+vim.pack.add({
+  "https://github.com/sainnhe/gruvbox-material",
+  "https://github.com/windwp/nvim-autopairs",
+  "https://github.com/airblade/vim-gitgutter",
+  "https://github.com/numToStr/Comment.nvim",
+  "https://github.com/j-hui/fidget.nvim",
+  "https://github.com/tpope/vim-fugitive",
+  "https://github.com/ibhagwan/fzf-lua",
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/ray-x/go.nvim",
+})
+
+vim.lsp.enable({
+  "gopls",
+  "rust_analyzer",
+})
+
+local augroup = vim.api.nvim_create_augroup("mcfdn.init", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup,
+  pattern = { "markdown", "go" },
+  callback = function()
+    vim.opt_local.textwidth = 80
+    vim.opt_local.colorcolumn = "80"
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup,
+  pattern = { "rust" },
+  callback = function()
+    vim.opt_local.textwidth = 100
+    vim.opt_local.colorcolumn = "100"
+  end,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = augroup,
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+
+    if client then
+      if client.name == "rust_analyzer" then
+        client.server_capabilities.inlayHintProvider = nil
+        vim.lsp.inlay_hint.enable(false, { bufnr = ev.buf })
+      end
+
+      if client:supports_method("textDocument/completion") then
+        vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+      end
+    end
+
+    local opts = { silent = true, buffer = ev.buf }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "<leader>l", vim.lsp.buf.format, opts)
+    vim.keymap.set("i", "<C-f>", vim.lsp.completion.get, opts)
+  end,
+})
+
+local keymap_opts = { silent = true }
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", keymap_opts)
+vim.keymap.set("n", "<C-d>", "<C-d>zz", keymap_opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", keymap_opts)
+vim.keymap.set("n", "n", "nzz", keymap_opts)
+vim.keymap.set("n", "N", "Nzz", keymap_opts)
+vim.keymap.set("v", "<", "<gv", keymap_opts)
+vim.keymap.set("v", ">", ">gv", keymap_opts)
+vim.keymap.set("v", "y", "ygv<Esc>", keymap_opts)
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", keymap_opts)
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", keymap_opts)
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", keymap_opts)
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", keymap_opts)
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, keymap_opts)
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, keymap_opts)
+vim.keymap.set("n", "<leader>a", ":Git blame<CR>", keymap_opts)
+
+vim.g.gruvbox_material_background = "medium"
+vim.cmd.colorscheme("gruvbox-material")
+
+require("nvim-autopairs").setup()
+require("Comment").setup()
+require("fidget").setup()
+require("go").setup()
+
+vim.api.nvim_create_user_command("TSInstallMine", function()
+  require("nvim-treesitter").install({ "go", "rust" })
+end, {})
+
+vim.api.nvim_create_autocmd("PackChanged", {
+  group = augroup,
+  callback = function(ev)
+    if ev.data.kind ~= "install" and ev.data.kind ~= "update" then
+      return
+    end
+    if ev.data.spec.name == "nvim-treesitter" then
+      require("nvim-treesitter").update()
+    end
+  end,
+})
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup,
+  pattern = { "go", "rust" },
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
+})
+
+require("fzf-lua").setup({
+  defaults = {
+    file_icons = false,
+    git_icons = false,
+  },
+  fzf_opts = { ["--cycle"] = true },
+})
+vim.keymap.set("n", "<leader>sf", function() require("fzf-lua").files() end, keymap_opts)
+vim.keymap.set("n", "<leader>f", function() require("fzf-lua").live_grep() end, keymap_opts)
+vim.keymap.set("n", "<leader><space>", function() require("fzf-lua").buffers() end, keymap_opts)
