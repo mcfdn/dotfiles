33ede22ce41f697f3e51aa58f12ff60ea643f196
James McFadden <998911+mcfdn@users.noreply.github.com>
2026-05-03 09:54:10 +0100

    remove old nvim config

--- .config/nvim/lua/plugins/theme.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.g.gruvbox_material_background = "medium"
-        vim.cmd.colorscheme("gruvbox-material")
-    end,
-}
