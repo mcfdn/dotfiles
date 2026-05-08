11952bd8430017dfb1ecdb9e1564e2db3c923312
James McFadden <998911+mcfdn@users.noreply.github.com>
2026-05-08 18:42:05 +0100

    autoindent is true by default

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,7 +6,6 @@ vim.g.maplocalleader = " "
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
-vim.o.autoindent = true
 vim.o.smartindent = true
 vim.o.wrap = false
 vim.o.cursorline = true
